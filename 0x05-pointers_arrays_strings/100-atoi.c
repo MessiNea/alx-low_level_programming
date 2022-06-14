c3f8d152955ced4b42e13d9c89c2624c93903eae
MessiNea <natnaelgirmaejigu@gmail.com>
2022-06-14 11:24:05 -0700

    100 atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * reset_to_98 - Updates the value it points to 98
- * @n: pointer to n
+ * print_rev - Prints a string in reverse
+ * @s: The string to print
  *
  * Return: void
  */
-void reset_to_98(int *n)
+void print_rev(char *s)
 {
-	*n = 98;
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+
+	_putchar('\n');
 }
