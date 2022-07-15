93ae4220b00ec3bba073132a0139f8c204d2d4b0
MessiNea <natnaelgirmaejigu@gmail.com>
2022-07-14 17:50:30 -0700

    dddaeo

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,22 +1,63 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * list_len - Show the number of elements of a list
- * @h: A linked list
+ * add_node_end - Adds a new node at the end of a list
+ * @head: The original linked list
+ * @str: The string to add to the node
  *
- * Return: The number of elements of a list
+ * Return: The address of the new list or NULL if it failed
  */
-size_t list_len(const list_t *h)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	size_t count = 0;
+	list_t *new_list, *temp;
 
-	while (h)
+	if (str != NULL)
 	{
-		h = h->next;
-		count++;
+		new_list = malloc(sizeof(list_t));
+		if (new_list == NULL)
+			return (NULL);
+
+		new_list->str = strdup(str);
+		new_list->len = _strlen(str);
+		new_list->next = NULL;
+
+		if (*head == NULL)
+		{
+			*head  = new_list;
+			return (*head);
+		}
+		else
+		{
+			temp = *head;
+			while (temp->next)
+				temp = temp->next;
+
+			temp->next = new_list;
+			return (temp);
+		}
 	}
 
-	return (count);
+	return (NULL);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (*s)
+	{
+		s++;
+		c++;
+	}
+
+	return (c);
 }
